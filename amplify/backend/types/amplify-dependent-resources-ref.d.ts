ec1430d2bb7adbdfda177304a92ae7f702f5a204
Sean Coyne <scoyne@goodrx.com>
2022-04-15 21:34:11 -0700

    v4

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s377a3a74f": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
